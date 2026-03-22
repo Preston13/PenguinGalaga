9373edd97733ff683310c5f690a0f501116f8947
Preston Grau <preston.grau@gmail.com>
2026-03-22 17:17:41 -0500

    initial commit

---
+++ Source/PenguinGalaga/PenguinGalaga.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PenguinGalaga.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PenguinGalaga, "PenguinGalaga" );
